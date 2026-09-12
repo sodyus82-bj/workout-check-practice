ab4b53a8423c5e62e4903cb062256b7c0b0a2d12
sodyus82-bj <328187438+sodyus82-bj@users.noreply.github.com>
2026-09-12 15:58:55 +0900

    모바일 캐시 및 로그인 화면 수정

--- service-worker.js
+++ service-worker.js
@@ -1,56 +1,56 @@
-const CACHE_NAME = "workout-check-v1";
+const CACHE_NAME = "workout-check-v2";
 
 const APP_FILES = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./manifest.json",
-  "./images/routine-guide.png",
-  "./images/icon-192.png",
-  "./images/icon-512.png"
+    "./",
+    "./index.html",
+    "./style.css?v=2",
+    "./script.js?v=2",
+    "./manifest.json",
+    "./images/routine-guide.png",
+    "./images/icon-192.png",
+    "./images/icon-512.png"
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES))
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES))
+    );
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((names) =>
-      Promise.all(
-        names
-          .filter((name) => name !== CACHE_NAME)
-          .map((name) => caches.delete(name))
-      )
-    )
-  );
+    event.waitUntil(
+        caches.keys().then((names) =>
+            Promise.all(
+                names
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            )
+        )
+    );
 });
 
 self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
+    if (event.request.method !== "GET") return;
 
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const copy = response.clone();
+    event.respondWith(
+        fetch(event.request)
+            .then((response) => {
+                const copy = response.clone();
 
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, copy);
-        });
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, copy);
+                });
 
-        return response;
-      })
-      .catch(() =>
-        caches.match(event.request).then((cachedResponse) => {
-          if (cachedResponse) return cachedResponse;
+                return response;
+            })
+            .catch(() =>
+                caches.match(event.request).then((cachedResponse) => {
+                    if (cachedResponse) return cachedResponse;
 
-          if (event.request.mode === "navigate") {
-            return caches.match("./index.html");
-          }
-        })
-      )
-  );
+                    if (event.request.mode === "navigate") {
+                        return caches.match("./index.html");
+                    }
+                })
+            )
+    );
 });
