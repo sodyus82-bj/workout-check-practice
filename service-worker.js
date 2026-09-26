6b24c6a470b23ccaaa6b9fffe6879be0afc4fbc3
sodyus82-bj <328187438+sodyus82-bj@users.noreply.github.com>
2026-09-26 19:32:43 +0900

    v40 운동 카드 버튼 색상 조정 및 관리자 영상 가로폭 수정

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
-const CACHE_NAME = "workout-check-v39";
+const CACHE_NAME = "workout-check-v40";
 
 const APP_FILES = [
   "./",
   "./index.html",
-  "./style.css?v=39",
-  "./exercise-data.js?v=39",
-  "./routine-components.js?v=39",
-  "./script.js?v=39",
+  "./style.css?v=40",
+  "./exercise-data.js?v=40",
+  "./routine-components.js?v=40",
+  "./script.js?v=40",
   "./manifest.json",
   "./images/routine-guide.png",
   "./images/center-logo.png",
